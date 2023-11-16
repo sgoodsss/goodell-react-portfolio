333390a4158959935e873ecb9d79c3f21eb619a1
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-11-16 17:59:24 -0500

    Added styling to Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,15 +1,13 @@
 // WHEN I am presented with the Resume section
 // THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
 import goodellResume from "/src/assets/Sarah_Goodell_React_Portfolio_Resume.pdf"
-import {Link} from 'react-router-dom'
+import "./Resume.css"
 
 export default function Resume() {
     return (
-      <div>
+      <div className="resumeCenter">
         <h1>Resume</h1>
-        <p>
-          Click the link below to download my Resume!
-        </p>
+        <h3>Click the link below to download my Resume!</h3>
         <a className="clickDownload" href={goodellResume} download> Click to download</a>
       </div>
     );
