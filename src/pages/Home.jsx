e1039c5ab272034cc84d9dc06df6d1a7ba4c1c99
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-11-16 15:12:46 -0500

    Added in a photo of myself to my Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@
 export default function Home() {
   return (
     <div>
-      <h1>Sarah Goodell</h1>
-      {/* Photo of me here */}
+      <h1 className="myName">Sarah Goodell</h1>
+      <img className="selfie" src="/src/assets/selfie.jpg" alt="Photo of Sarah Goodell" />
       <p>
-        Hello, I'm Sarah Goodell, a passionate Full Stack Web Developer dedicated to crafting engaging digital experiences. My journey into the world of coding began after a fulfilling stint in Secondary Education, specializing in English/Language Arts, at the University of Central Florida. While teaching was rewarding, my fascination with software development grew stronger with time, prompting me to transition and pursue my true passion. <br />
+        Hello, I'm Sarah Goodell, a passionate Full Stack Web Developer dedicated to crafting engaging digital experiences. My journey into the world of coding began after a fulfilling stint in Secondary Education specializing in English/Language Arts. While teaching was rewarding, my fascination with software development grew stronger with time, prompting me to transition and pursue my true passion. <br />
         <br />
         The skills honed during my teaching years have seamlessly translated into my development journey, allowing me to blend creativity, problem-solving, and effective communication in my projects. I strongly believe that my teaching background brings a unique perspective to the table, enabling me to understand user needs and create intuitive, user-friendly interfaces. <br />
         <br />
