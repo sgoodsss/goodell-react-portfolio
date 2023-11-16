724103f2f1fd827877e14591a0cd9293e1d2a240
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-11-16 14:27:18 -0500

    Added in project images to Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,44 +1,47 @@
 // WHEN I am presented with the Portfolio section
 // THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repository
 import BookNite from "/src/assets/Book _Nite_Generator.png"
+import LeapToSuccess from "/src/assets/leap-to-success.png"
+import JATE from "/src/assets/JATE.png"
+import techTalk from "/src/assets/techTalk.png"
+import weatherDashboard from "/src/assets/weatherDashboard.png"
+import passwordGenerator from "/src/assets/passwordGenerator.png"
 import "./Projects.css"
-// ./assets/Book_Nite_Generator.png
-// ./assets/Book_Nite_Generator.png
-// /assets/Book_Nite_Generator.png
-// assets/Book_Nite_Generator.png
 
 export default function Projects() {
 
   const projects = [
     {
       id: 1,
-      title: 'Project 1',
-      imgsrc: BookNite
+      title: 'Leap To Success',
+      imgsrc: LeapToSuccess,
+      linkToApp: "",
+      linkToGithub: ""
     },
     {
       id: 2,
-      title: 'Project 1',
+      title: 'Book Nite Generator',
       imgsrc: BookNite
     },
     {
       id: 3,
-      title: 'Project 1',
-      imgsrc: BookNite
+      title: 'Just Another Text Editor',
+      imgsrc: JATE
     },
     {
       id: 4,
-      title: 'Project 1',
-      imgsrc: BookNite
+      title: 'Tech Talk!',
+      imgsrc: techTalk
     },
     {
       id: 5,
-      title: 'Project 1',
-      imgsrc: BookNite
+      title: '5 Day Weather Forecast',
+      imgsrc: weatherDashboard
     },
     {
       id: 6,
-      title: 'Project 1',
-      imgsrc: BookNite
+      title: 'Password Generator',
+      imgsrc: passwordGenerator
     }
   ]
 
@@ -54,11 +57,12 @@ export default function Projects() {
         }}>
 
         {projects.map(project => (
+          <a href="">
           <div className="card" key={project.id}>
             <h2>{project.title}</h2>
             <img src={project.imgsrc} alt="project image" />
           </div>
-          
+          </a>
         ))}
 
       </div>
