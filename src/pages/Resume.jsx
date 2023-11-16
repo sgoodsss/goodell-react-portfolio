2860f872aae7aed82e009fd4c0b2f626171e1863
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-11-16 17:52:52 -0500

    Fixed Resume Download bug

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,7 @@
 // WHEN I am presented with the Resume section
 // THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
 import goodellResume from "/src/assets/Sarah_Goodell_React_Portfolio_Resume.pdf"
+import {Link} from 'react-router-dom'
 
 export default function Resume() {
     return (
@@ -9,7 +10,7 @@ export default function Resume() {
         <p>
           Click the link below to download my Resume!
         </p>
-        <a href={goodellResume} download>Click to download</a>
+        <a className="clickDownload" href={goodellResume} download> Click to download</a>
       </div>
     );
   }
