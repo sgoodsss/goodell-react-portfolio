5362bc5aa77539b252ffa554925255365ce03c2a
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-11-16 15:35:27 -0500

    Added styling to Home page of Portfolio

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -6,7 +6,7 @@ function NavTabs() {
   const currentPage = useLocation().pathname;
 
   return (
-    <ul className="nav nav-tabs">
+    <ul className="nav nav-underline">
       <li className="nav-item">
         <Link
           to="/"
