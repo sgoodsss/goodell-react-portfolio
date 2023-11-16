a9ca871ffc7ca838cc284adc781ccfe4848f1074
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-11-16 16:11:26 -0500

    Tried to import my resume so that it is downloadable for the user

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,6 @@
 // WHEN I am presented with the Resume section
 // THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
+import goodellResume from "/src/assets/Sarah_Goodell_React_Portfolio_Resume.pdf"
 
 export default function Resume() {
     return (
@@ -8,7 +9,7 @@ export default function Resume() {
         <p>
           Click the link below to download my Resume!
         </p>
-        <a href='/somefile.txt' download>Click to download</a>
+        <a href={goodellResume} download>Click to download</a>
       </div>
     );
   }
